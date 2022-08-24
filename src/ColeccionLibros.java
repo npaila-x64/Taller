8a4ad614316aead3878b13c402bbfe73b0cc1690
N. Paila <n.paila01@ufromail.cl>
2022-08-24 09:11:34 -0400

    agrega estructura a método agregarLibro() y a mostrarTodaColeccion()

--- src/ColeccionLibros.java
+++ src/ColeccionLibros.java
@@ -8,15 +8,34 @@ public class ColeccionLibros {
         agregarLibro(libros,"Cujo","Stephen King","Ed. Que susto");
         agregarLibro(libros,"Un mundo feliz","Aldous Huxley","Ed. No Me Acuerdo");
 
-        menu();
+        menu(libros);
     }
 
-    public static void menu() {
+    public static void menu(String[][] misLibros) {
+
+        mostrarTodaColeccion(misLibros);
 
     }
 
-    public static String[][] agregarLibro(String[][] misLibros, String titulo, String autor, String editorial) {
-        return new String[][]{};
+    public static void agregarLibro(String[][] misLibros, String titulo, String autor, String editorial) {
+
+        if (estaLaColeccionLlena(misLibros)) {
+            System.out.println("La colección esta llena.");
+            return;
+        }
+
+        for (int i = 0; i < misLibros.length; i++) {
+            if (misLibros[i][0] == null) {
+                misLibros[i][0] = autor;
+                misLibros[i][1] = titulo;
+                misLibros[i][2] = editorial;
+                break;
+            }
+        }
+    }
+
+    public static boolean estaLaColeccionLlena(String[][] misLibros) {
+        return misLibros[99][0] != null;
     }
 
     public static int totalLibros(String[][] misLibros) {
@@ -40,7 +59,12 @@ public class ColeccionLibros {
     }
 
     public static void mostrarTodaColeccion(String[][] misLibros) {
-
+        for (int i = 0; i < misLibros.length; i++) {
+            if (misLibros[i][0] == null) {
+                break;
+            }
+            System.out.println(misLibros[i][0] + ", " + misLibros[i][1] + ", " + misLibros[i][2]);
+        }
     }
 }
 
